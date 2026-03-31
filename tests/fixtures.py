6fa22022870daee200e7d93169a99ea1336fae90
Pavel Vodochnikov <pavelvodocnikov@gmail.com>
2026-03-31 23:46:06 +0500

    tests: add check PXOLLY_API_TOKEN in fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,8 +9,11 @@ from pxolly_api import PxollyAPI
 dotenv.load_dotenv()
 
 
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture()
 async def pxolly_api() -> typing.AsyncGenerator[PxollyAPI]:
     token = os.getenv("PXOLLY_API_TOKEN")
+    if not token:
+        raise ValueError("Not PXOLLY_API_TOKEN in .env file")
+
     async with PxollyAPI(token=token) as api:
         yield api
