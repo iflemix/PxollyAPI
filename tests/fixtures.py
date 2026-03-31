06ffc4d01ccf79006f7b4814ce44f2ccb6051423
Pavel Vodochnikov <pavelvodocnikov@gmail.com>
2026-03-31 21:54:17 +0500

    tests: fixture to asyncio and generator for close api instance

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,15 +1,16 @@
 import os
+import typing
 
 import dotenv
-import pytest
+import pytest_asyncio
 
 from pxolly_api import PxollyAPI
 
 dotenv.load_dotenv()
 
 
-@pytest.fixture()
-def pxolly_api() -> PxollyAPI:
+@pytest_asyncio.fixture
+async def pxolly_api() -> typing.AsyncGenerator[PxollyAPI]:
     token = os.getenv("PXOLLY_TOKEN")
-    api = PxollyAPI(token=token)
-    return api
+    async with PxollyAPI(token=token) as api:
+        yield api
