60e54fc96eefe92b600f19006369adf29ebe0e73
Pavel Vodochnikov <pavelvodocnikov@gmail.com>
2026-04-01 01:01:44 +0500

    tests: add users methods tests

---
+++ tests/methods/test_users.py
@@ -0,0 +1,16 @@
+import pytest
+
+from pxolly_api import PxollyAPI
+from pxolly_api.models.users import GetUserRegisterdDate, GetUserStickerPacks
+
+
+@pytest.mark.asyncio
+async def test_users_get_registered_date(pxolly_api: PxollyAPI) -> None:
+    result = await pxolly_api.users.get_registered_date("733772362")
+    assert isinstance(result, GetUserRegisterdDate)
+
+
+@pytest.mark.asyncio
+async def test_users_get_sticker_packs(pxolly_api: PxollyAPI) -> None:
+    result = await pxolly_api.users.get_sticker_packs("733772362", max_count=30)
+    assert isinstance(result, GetUserStickerPacks)
